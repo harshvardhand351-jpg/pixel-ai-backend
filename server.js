4e57a91860ee59a22e8f340092c5c5d6558c6e3d
Harsh <harshvardhand351@gmail.com>
2026-05-06 15:15:22 +0530

    fixed fetch syntax

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/chat", async (req, res) => {
 
   try {
     const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
